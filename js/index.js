eb7202b3f1628b6014db641e6bb0c36d99e45a61
Spencer Douglas <cyrun2@gmail.com>
2021-08-04 20:49:54 -0400

    Added carousel and added actual text to experience. Also cleaned up some navbar coding.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-import anime from 'animejs/lib/anime.es';
+//import anime from 'animejs/lib/anime.es';
 
-anime({
-    targets: '.line-drawing-demo .lines path',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'easeInOutSine',
-    duration: 1500,
-    delay: function (el, i) { return i * 250 },
-    direction: 'alternate',
-    loop: true
-}); 
+// anime({
+//     targets: '.line-drawing-demo .lines path',
+//     strokeDashoffset: [anime.setDashoffset, 0],
+//     easing: 'easeInOutSine',
+//     duration: 1500,
+//     delay: function (el, i) { return i * 250 },
+//     direction: 'alternate',
+//     loop: true
+// }); 
 
 
 
